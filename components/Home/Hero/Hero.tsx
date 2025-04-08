65235a6a070c3867594f5dd1cb947f652ec41938
福福 <evisu@fufudeMacBook-Pro.local>
2025-04-08 13:25:40 +0800

    test github

--- components/Home/Hero/Hero.tsx
+++ components/Home/Hero/Hero.tsx
@@ -26,7 +26,7 @@ const Hero = () => {
                ease-out duration-300">
             <span className='absolute right-0 w-8 h-32 -mt-12 transition-all duration-1000 transform translate-x-12
              bg-white opacity-10 rotate-12 group-hover:-translate-x-40 ease '></span>
-            <span className='relative font-bold'>Search</span>
+            <span className='relative font-bold'>Search your Trip</span>
             </Link>
             </div>
 
